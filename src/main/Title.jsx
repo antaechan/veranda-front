e3ccca9567a4f217b461b01c3dec8662c42294d7
antaechan <0627antaechan@gmail.com>
2022-08-15 21:37:36 +0900

    Add V-logo in title

--- src/main/Title.jsx
+++ src/main/Title.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./css/Title.css";
 
 function Title(props) {
-  return <div className="Title">Title</div>;
+  return (
+    <div className="Title">
+      <img src="v-Logo.png" className="vLogo" />
+    </div>
+  );
 }
 
 export default Title;
