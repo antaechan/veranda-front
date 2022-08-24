b3d98fcc8941d5389df74ec2d5caf818b226a4cd
antaechan <0627antaechan@gmail.com>
2022-08-24 22:27:47 +0900

    Implement number display in page navigator, postButton

--- src/main/components/PhotoCard.jsx
+++ src/main/components/PhotoCard.jsx
@@ -7,20 +7,31 @@ const { Meta } = Card;
 const PhotoCard = ({ zine }) => {
   const { title, src, alt, url } = zine;
   return (
-    <StyledLink to={url}>
+    <div to={url}>
       <Card
         hoverable
         cover={
           <img
             alt={alt}
             src={src}
-            style={{ height: "100%", width: "100%", objectFit: "contain" }}
+            style={{
+              height: "100%",
+              width: "100%",
+              objectFit: "contain",
+            }}
           />
         }
+        style={{
+          backgroundColor: `rgb(255, 252, 246)`,
+          border: "none",
+        }}
       >
-        <Meta title={title} />
+        <Meta
+          title={title}
+          style={{ fontFamily: "Times New Roman", fontWeight: "bold" }}
+        />
       </Card>
-    </StyledLink>
+    </div>
   );
 };
 
--- src/main/pages/Zines.jsx
+++ src/main/pages/Zines.jsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import PhotoCard from "../components/PhotoCard";
 import axios from "axios";
+import StyledLink from "./../components/StyledLink";
+
+import "./css/Zines.css";
 
 const ZineContainer = styled.div`
   box-sizing: border-box;
@@ -15,23 +18,59 @@ const sampleZine = {
 };
 
 const Zines = () => {
-  const [zines, setZines] = useState(null);
+  const [zines, setZines] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [pageNumber, setPageNumber] = useState(1);
+  const zinesInPage = 12;
 
   useEffect(() => {
-    const fetchZines = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get("url");
-        setZines(response.data.zines);
-      } catch (error) {
-        console.log(error);
-      }
-      setLoading(false);
-    };
+    // const fetchZines = async () => {
+    //   setLoading(true);
+    //   try {
+    //     const response = await axios.get("url");
+    //     setZines(response.data.zines);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    //   setLoading(false);
+    // };
+    // fetchZines();
 
-    fetchZines();
+    setZines([
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+      sampleZine,
+    ]);
   }, []);
+
+  function range(start, end) {
+    let array = [];
+    for (let i = start; i <= end; ++i) {
+      array.push(i);
+    }
+    return array;
+  }
+
+  const pageEnd = Math.ceil(zines.length / zinesInPage);
+  const pageList = range(1, pageEnd);
+
+  const zinesAtPage = zines.slice(
+    zinesInPage * (pageNumber - 1),
+    zinesInPage * pageNumber
+  );
 
   if (loading) {
     return <ZineContainer>로딩 중...</ZineContainer>;
@@ -41,16 +80,50 @@ const Zines = () => {
     return null;
   }
 
+  const PageNavigator = () => {
+    // const prevPageNumber =
+    //   pageNumber === 1 ? pageNumber : setPageNumber(pageNumber - 1);
+    // const nextPageNumber =
+    //   pageNumber === pageEnd ? pageNumber : setPageNumber(pageNumber + 1);
+    var prevURL = `/zine/${pageNumber}`;
+    var nextURL = `/zine/${pageNumber}`;
+
+    return (
+      <div className="pageNavigator" onClick={() => {}}>
+        <StyledLink to={String(pageNumber)} className="previousButton">
+          Previous
+        </StyledLink>
+        {pageList.map((pageNumber) => {
+          const pageURL = `/zine/${pageNumber}`;
+          return (
+            <StyledLink to={pageURL} className="pageNumberButton">
+              {pageNumber}
+            </StyledLink>
+          );
+        })}
+        <StyledLink to={String(pageNumber)} className="nextButton">
+          Next
+        </StyledLink>
+      </div>
+    );
+  };
+
   return (
-    <ZineContainer>
-      <Row gutter={[16, 16]}>
-        {zines.map((zine) => (
-          <Col xs={12} sm={12} md={8} lg={6} xl={6}>
-            <PhotoCard zine={zine} />
-          </Col>
-        ))}
-      </Row>
-    </ZineContainer>
+    <div className="zineContent">
+      <ZineContainer>
+        <Row gutter={[16, 16]}>
+          {zinesAtPage.map((zine) => (
+            <Col xs={12} sm={12} md={8} lg={6} xl={6}>
+              <PhotoCard zine={zine} />
+            </Col>
+          ))}
+        </Row>
+      </ZineContainer>
+      <PageNavigator />
+      <StyledLink to="/edit" className="postButton" onClick={() => {}}>
+        ✍
+      </StyledLink>
+    </div>
   );
 };
 
